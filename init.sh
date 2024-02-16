648d4f8c90f6cae08146b5acb89f16a88390e1b2
Troublesis <40723560+Troublesis@users.noreply.github.com>
2024-02-16 22:01:58 +1100

    Update init.sh add install vim and remove -y inside script

--- init.sh
+++ init.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Troublesis/CentOS-Init/main/init.sh)" -y
 
 # Update system
 echo "Updating system..."
-yum update -y
+yum update
+
+# Install Python 3
+echo "Installing Vim..."
+yum install vim
 
 # Install Python 3
 echo "Installing Python 3..."
-yum install python3 -y
+yum install python3
 
 # Install Make
 echo "Installing Make..."
-yum install make -y
+yum install make
 
 # Install Git
 echo "Installing Git..."
-yum install git -y
+yum install git
 
 # Install Zsh
 echo "Installing Zsh..."
-yum install zsh -y
+yum install zsh
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Change default shell to Zsh
 echo "Changing default shell to Zsh..."
-chsh -s $(which zsh) -y
+chsh -s $(which zsh)
